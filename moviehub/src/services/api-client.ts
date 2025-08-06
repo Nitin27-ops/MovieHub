73e0567fe5514321892f7c46b99652a3f2d3f13b
Nitin27-ops <nitinnegi7819@gmail.com>
2025-08-06 10:02:02 +0530

    Update api-client.ts

--- moviehub/src/services/api-client.ts
+++ moviehub/src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL:"https://api.themoviedb.org/3",
     params:{
-        api_key:"41ee65d3c2f3548562a9c4ca2c326bd9",
+        api_key:""
     }
 })
